792d9d81f8bb8736111ea4ab50bfbfe7b56456a6
Ivan5 <converse_ivan@hotmail.com>
2019-07-09 17:57:59 -0500

    Add conection to the database in mongo

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const app = express();
 
 const config = require("./config");
 
+const db = require("./config/database");
+
+const cors = (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+};
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.listen(
   config.port,
   console.log("Server has started on port %s", config.port)
