0c3013665279531b41f48681eb9f481d128f4a70
Ivan5 <converse_ivan@hotmail.com>
2019-07-10 17:25:58 -0500

    Add model and control of user

--- routes/user/controller.js
+++ routes/user/controller.js
@@ -5,6 +5,22 @@ module.exports = {
     res.status(200).send({ msg: "Login Success" });
   },
   register: (req, res) => {
-    res.status(200).send({ msg: "Register succesfully" });
+    let newUser = new model({
+      forename: req.body.forename,
+      surename: req.body.surename,
+      email: req.body.email,
+      password: req.body.password
+    });
+
+    newUser
+      .save()
+      .then(result => {
+        console.log(result);
+        res.status(200).send({ msg: "Register succesfully", user_id: "id" });
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).send({ msg: "Register Unsuccesfully" });
+      });
   }
 };
--- routes/user/model.js
+++ routes/user/model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = mongoose.Schema({
   forename: {
@@ -22,3 +23,29 @@ const userSchema = mongoose.Schema({
     default: Date.now()
   }
 });
+
+const userModel = mongoose.model("user", userSchema);
+userSchema.pre("save", next => {
+  let user = this;
+  if (!user.isModified("password")) return next();
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) return next(err);
+
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+module.exports = userModel;
